4cc1a553e0d5a8b5f7f6ed4789bdd8272ef4819d
ximinez <ximinez@users.noreply.github.com>
2023-10-19 18:49:07 +0000

    deploy: 5af9dc5abd5ba4dc955a6eb96701dea390c636e8

--- search/classes_19.js
+++ search/classes_19.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['yocto_16285',['yocto',['http://en.cppreference.com/w/cpp/numeric/ratio/ratio.html',1,'std']]],
-  ['yotta_16286',['yotta',['http://en.cppreference.com/w/cpp/numeric/ratio/ratio.html',1,'std']]]
+  ['yocto_16301',['yocto',['http://en.cppreference.com/w/cpp/numeric/ratio/ratio.html',1,'std']]],
+  ['yotta_16302',['yotta',['http://en.cppreference.com/w/cpp/numeric/ratio/ratio.html',1,'std']]]
 ];
--- search/pages_8.js
+++ search/pages_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['json_28773',['JSON',['../md_ripple_json_README.html',1,'']]],
-  ['json_20todo_28774',['JSON TODO',['../md_ripple_json_TODO.html',1,'']]]
+  ['json_28799',['JSON',['../md_ripple_json_README.html',1,'']]],
+  ['json_20todo_28800',['JSON TODO',['../md_ripple_json_TODO.html',1,'']]]
 ];
